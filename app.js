321eaa136fecc1b780187381396eb8689f7cbe16
PerdidoenCoding <willianlinchang@gmail.com>
2023-12-13 17:29:09 +0800

    Added function for mouseover drawing, color picker, choose color, eraser and clear.

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-
-
-
-
 const grid = document.getElementById("grid");
 const sizeValue = document.getElementById('sizeValue');
 const sizeSlider = document.getElementById('sizeSlider');
+const colorPicker = document.getElementById('color-picker');
+const colorButton = document.getElementById('color-button');
+const clearButton = document.getElementById('clear-button')
+const eraserButton = document.getElementById('eraser-button')
 
 let currentSize = 16;
+let mouseIsDown = false;
+let currentColor = '#000000';
+
+
 
 sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
 sizeSlider.onchange = (e) => changeSize(e.target.value)
+clearButton.onclick = () => reloadGrid()
+
+colorButton.addEventListener('click', function() {
+  currentColor = colorPicker.value;
+});
+
+eraserButton.addEventListener('click', function() {
+  currentColor = '#fefefe';
+});
 
 function setCurrentSize(newSize) {
     currentSize = newSize;
@@ -34,14 +47,38 @@ function changeSize(value) {
     grid.innerHTML = '';
   }
 
+  grid.addEventListener('mousedown', function() {
+    mouseIsDown = true;
+});
+
+  grid.addEventListener('mouseup', function() {
+    mouseIsDown = false;
+});
+
+
 function makeGrid(size) {
   grid.style.gridTemplateColumns =`repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
-    grid.appendChild(cell).className = "grid-item";
+    cell.addEventListener('mouseover', function() {
+      if(mouseIsDown) {
+      this.style.backgroundColor = currentColor;
+      }
+
+    cell.addEventListener('touchstart', function() {
+        this.style.backgroundColor = currentColor;
+    });
+
+    cell.addEventListener('mousedown', function() {
+      this.style.backgroundColor = currentColor;
+  });
+  });
+
+  grid.appendChild(cell).className = "grid-item";
   };
-  
 };
+
+
 
 makeGrid(16);
